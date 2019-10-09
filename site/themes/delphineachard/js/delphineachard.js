73c60b86f6779a08d3f093e016db8980ff80c369
Gilles FRANCOIS <gilles@sopress.net>
2019-10-09 12:46:27 +0200

    docs

--- site/themes/delphineachard/js/delphineachard.js
+++ site/themes/delphineachard/js/delphineachard.js
@@ -1,4 +1,5 @@
 var sti=false;
+var illustration_originale=false;
 $(document).ready(function() {
 	$('.albums a').each(function() {
 		if($(this).data('illustration')) {
@@ -8,36 +9,58 @@ $(document).ready(function() {
 		}
 	})
 	if($('.albums').length) {
-		$('.illustration').data('background-image',$('.illustration').css('background-image'));
+		illustration_originale = document.querySelector('.illustration').outerHTML;
 		$('.illustration').addClass('visible');
 
 	}
 
 
-
 	$('.albums a').on('mouseover',function() {
 		if($(this).data('illustration')) {
 			reset_illustration();
+			console.log($(this).data('afficher_en_plein_ecran'))
+
+
 			sti = setTimeout(() => {
 				let illustration = $(this).data('illustration');
+
+				let classes = 'visible';
+				console.log($(this).data('afficher_en_plein_ecran'))
+
+				if($(this).data('afficher_en_plein_ecran')) {
+					classes+=' illustration-fullscreen';
+				}
+				if($(this).data('couleur_de_fond') == 'white') {
+					classes+=' has-background-white';
+				} else {
+					classes+=' has-background-black';
+				}
+
 				if(illustration.includes('mp4')) {
 					$('.illustration').html(video_code(illustration));
 				} else {
 					$('.illustration').css('background-image','url('+illustration+')');
 				}
-				$('.illustration').addClass('visible');
+				$('.illustration').addClass(classes);
 			}, 300);
 		}
 	}).on('mouseout', () => {
 		reset_illustration()
 		sti = setTimeout(() => {
-			$('.illustration').css('background-image',$('.illustration').data('background-image'))
+			$('.illustration').replaceWith(illustration_originale);
+//			$('.illustration').css('background-image',$('.illustration').data('background-image'))
 			$('.illustration').addClass('visible');
 		}, 300);
 	})
 
 
 })
+$(window).load(function () {
+	document.querySelectorAll('video').forEach(function(video) {
+		video.muted = "muted";
+	});
+});
+
 
 function reset_illustration() {
 	if(sti) {
@@ -45,7 +68,7 @@ function reset_illustration() {
 	}
 	$('.illustration').css('background-image','')
 	$('.illustration').html('');
-	$('.illustration').removeClass('visible');
+	$('.illustration').removeClass('visible illustration-fullscreen has-background-black has-background-white');
 }
 
 
